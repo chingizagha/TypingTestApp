894afd888500293bd51daae36e387869dc54247d
chingizagha <cingiz167@gmail.com>
2023-12-01 14:13:06 +0400

    update

--- script.js
+++ script.js
@@ -81,70 +81,88 @@ const wordsSet = [
     'around',
     'too',
 ];
+
+let randomArr = [];
 let divWord = document.querySelector('.div-word')
-function showWords(){
-    fetch("https://random-words-api.vercel.app/word", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "54c13cbeb8msh05768ef5da7c7a4p132975jsn9727f17e4753",
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+
+for(let i=0; i<wordsSet.length; i++){
+    randomArr.push(i);
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+  
+    return array;
+  }
 
-
-
+function showWords(){
+    shuffle(randomArr);
     divWord.innerHTML = ' ';
     for(let i=0; i<wordsSet.length; i++){
-        const html = `<span class="word-box">${wordsSet[i]}</span>`
+        const html = `<span class="word-box">${wordsSet[randomArr[i]]}</span>`
         divWord.insertAdjacentHTML('beforeend', html);
     }
 }
 showWords();
+
+
 let wordSpan = document.querySelectorAll('.word-box')
 let inputPlace = document.querySelector('.input-place')
 let btnReload = document.querySelector('.btn-reload')
 let scoreBox = document.querySelector('.score-box')
 let timeBox = document.querySelector(".timer-box")
 let divResult = document.querySelector('.div-result')
- 
+wordSpan[0].style.backgroundColor = '#ddd';
+
+let randomNumber = 0; 
 let isReloaded = false;
 let i = 0;
 let count = 0;
 let timer;
 let timerExecuted = false;
-let timeBoxColor = false;
-wordSpan[i].style.backgroundColor = '#ddd';
+let timeBoxColor = false; 
+
+function getAllSpan(){
+    wordSpan = document.querySelectorAll('.word-box')
+}
+
 
 inputPlace.addEventListener('keyup', function(e){
-    if(e.target.value != wordsSet[i].substring(0, e.target.value.length)){
+    if(e.target.value != wordSpan[i].textContent.substring(0, e.target.value.length)){
         wordSpan[i].style.color = 'red';
         wordSpan[i].style.backgroundColor = '#ddd';
     }
-    if(e.target.value == wordsSet[i].substring(0, e.target.value.length)){
+    if(e.target.value == wordSpan[i].textContent.substring(0, e.target.value.length)){
         wordSpan[i].style.color = '#000';
         wordSpan[i].style.backgroundColor = '#ddd';
     }
-
     if(e.target.value.includes(' ') && inputPlace.value.length > 0){
-        if(wordsSet[i] === inputPlace.value.trim()){
+        if(wordSpan[i].textContent == inputPlace.value.trim()){
             ++count;
             wordSpan[i].style.backgroundColor = '#fff';
             wordSpan[i].style.color = 'green';
+            debugger;
             wordSpan[i+1].style.backgroundColor = '#ddd';
             inputPlace.value = '';
             ++i;
         }
-        else if(wordsSet[i] != inputPlace.value.length > 0){
+        else if(wordSpan[i].textContent != inputPlace.value.length > 0){
             wordSpan[i].style.color = 'red';
             wordSpan[i].style.backgroundColor = '#fff';
             wordSpan[i+1].style.backgroundColor = '#ddd';
+            debugger; 
             inputPlace.value = '';
             ++i;
         }
@@ -202,14 +220,13 @@ function showResult() {
         divWord.style.display = 'none';
         divResult.style.display = 'block';    
     }
-    
 }
 
 //Button for reloading everything
 btnReload.addEventListener('click', function(e){
     e.preventDefault();
     if(timer) clearInterval(timer)
-    wordSpan[i].style.backgroundColor = '#fff';
+    // wordSpan[i].style.backgroundColor = '#fff';
     timerExecuted = false;
     i = 0;
     count = 0;
@@ -219,9 +236,12 @@ btnReload.addEventListener('click', function(e){
         elem.style.color = 'black';
     }))
     inputPlace.value = "";
-    wordSpan[0].style.backgroundColor = '#ddd';
     isReloaded = true;
     showResult();
+    debugger;   
+    showWords();
+    getAllSpan();
+    wordSpan[0].style.backgroundColor = '#ddd';
 })
 //Button for hiding time in box
 timeBox.addEventListener('click', function(e){
@@ -234,7 +254,6 @@ timeBox.addEventListener('click', function(e){
         timeBox.style.color = "white";
         timeBoxColor = false;
     }
-
 })
 
 
